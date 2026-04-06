4394d43fd8e4d382394f47c7dfc74630a1af6428
Allyssan Marie <allyssan19@gmail.com>
2026-04-06 11:45:22 -0300

    Create ex01-variaveis.java
    feat: adiciona ex01 variaveis (java)

---
+++ java/basico/ex01-variaveis.java
@@ -0,0 +1,12 @@
+//Tipos de variáveis//
+
+public class Main {
+  public static void main(String[] args) {
+
+    int numero = 78;
+    float saldo = 56.9;
+    String nome = "Allyssan";
+
+    System.out.println("Hello world!");
+  }
+}
