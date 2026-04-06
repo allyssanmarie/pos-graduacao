798d4f571142fe23563da55d53649becb925080b
Allyssan Marie <allyssan19@gmail.com>
2026-04-06 13:05:55 -0300

    Create Ex08IfElse.java
    feat: ex 08 estr cond composta if else

---
+++ java/condicional/Ex08IfElse.java
@@ -0,0 +1,17 @@
+//if else - condicional composta//
+
+import java.util.*;
+
+public class Ex08IfElse {
+    public static void main(String[] args) {
+      int nota = 2;
+      
+      if (nota >= 5) {
+        System.out.println("Aprovado");
+      }
+      
+      else {
+        System.out.print("Reprovado");
+      }
+    }
+}
